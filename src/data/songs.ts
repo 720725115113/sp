2dcccec73d53fd7423523dfe60eec171b4100994
720725115113 <720725115113@hicet.ac.in>
2026-07-28 20:42:39 +0530

    Update songs.ts

--- src/data/songs.ts
+++ src/data/songs.ts
@@ -32,7 +32,7 @@ export const songs: Song[] = [
   color: "#8b5cf6"            // accent color (hex)
  },
   {
-  id: "s18",          // unique slug
+  id: "s19",          // unique slug
   title: "Kadhalikathey",        // display title
   artist: "Hiphop Tamizha, Kaushik Krish",
   album: "Imaikkaa Nodigal Tamil",        // optional
@@ -103,6 +103,19 @@ export const songs: Song[] = [
   coverUrl: "https://cdn.phototourl.com/free/2026-07-27-561cfcff-d7b7-4f0c-af83-ab164dd93037.jpg",  // 🖼️ THUMBNAIL LINK
   genre: "",
   year: 2026,
+  color: "#8b5cf6"            // accent color (hex)
+},
+
+{
+  id: "s20",          // unique slug
+  title: "Neeyum Naanum Anbe",        // display title
+  artist: "Raghu Dixit, D. Sathyaprakash, Jithin Raj",
+  album: "Imaikkaa Nodigal Tamil",        // optional
+  duration: "4:45",           // optional
+  audioUrl: "https://www.masstamilan.dev/downloader/1jIfBp1Rqv_0HeD7yzPsyg/1785337053/d128_cdn/16761/MTE3LjI0My4xMjIuMTAx",   // 🔗 SONG LINK
+  coverUrl: "https://imgs.search.brave.com/rpQUKP8QCO4xaulWOLDW6ZlxZO6C7Tyed0tEvam-PnU/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuZmlsbWliZWF0/LmNvbS9pbWcvcG9w/Y29ybi9mYW5faW1h/Z2VzL21vdmllLzE1/NDUxL2ltYWlra2Fh/LW5vZGlnYWwtcGhv/dG9zLWltYWdlcy02/MTk4NS5qcGc",  // 🖼️ THUMBNAIL LINK
+  genre: "",
+  year: 2018,
   color: "#8b5cf6"            // accent color (hex)
 },
 
