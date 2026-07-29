a32fec74f904259f1a311ec432cd30e2cd660902
720725115113 <720725115113@hicet.ac.in>
2026-07-29 21:54:50 +0530

    Register PWA Service Worker sw.js to enable 1-click Android APK generation and native home screen installation

--- public/sw.js
+++ public/sw.js
@@ -1,29 +1,41 @@
-const CACHE_NAME = 'wavelength-cache-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const CACHE_NAME = "wavelength-cache-v1";
+const ASSETS_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/app_icon.png",
+  "/favicon.png"
+];
 
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
   );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
   );
   self.clients.claim();
 });
 
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
   event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        const copy = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
-        return response;
-      })
-      .catch(() => caches.match(event.request).then(cacheResponse => cacheResponse || caches.match('/')))
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
   );
 });
